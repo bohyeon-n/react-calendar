5e6a06529d78fa6bc31f6fa23f803ae68e0e4910
koobohyeon <goosee115@gmail.com>
2018-10-03 17:52:46 +0900

    다음 달력 미리보기 추가

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -12,19 +12,46 @@ export default class Calendar extends React.Component {
     }
     return result;
   }
+  forward = e => {
+    const { month, year } = this.state;
+    if (month === 11) {
+      this.getDays(year + 1, 0);
+    } else {
+      this.getDays(year, month + 1);
+    }
+  };
+  backward = e => {
+    const { month, year } = this.state;
+    if (month === 0) {
+      this.getDays(year - 1, 11);
+    } else {
+      this.getDays(year, month - 1);
+    }
+  };
   getDays = (year, month) => {
     // 일수
     const days = new Date(year, month, 0).getDate();
-    console.log(days);
+    // 그 달의 1일 요일
     let day = new Date(year, month, 1).getDay();
     //day가 0이 아니면 (일요일) 전 달에서 그만큼 가져오기
     let arrays = [...Array(days)].map((value, index) => index + 1);
     if (day !== 0) {
       let days = new Date(year, month, 0).getDate();
       while (day > 0) {
-        arrays.unshift({ day: days, month: month });
+        arrays.unshift({ day: days, month });
         day--;
         days--;
+      }
+    }
+    // 마지막 일자가 토요일이 아니면 다음 달 가져오기
+    let lastDay = parseInt(new Date(year, month, days).getDay(), 10);
+
+    if (lastDay !== 6) {
+      let day = 1;
+      while (lastDay < 6) {
+        lastDay++;
+        arrays.push({ day });
+        day++;
       }
     }
     arrays = this.chunkArray(arrays, 7);
@@ -41,30 +68,39 @@ export default class Calendar extends React.Component {
   }
   week = ["일", "월", "화", "수", "목", "금", "토"];
   render() {
+    console.log(this.state);
     const { arrays, month, year } = this.state;
     return (
       <div className="container">
         <div className="month">
-          <button onClick={e => this.getDays(year, month - 1)}>앞으로</button>
-          <button>{month + 1}</button>
-          <button onClick={e => this.getDays(year, month + 1)}>뒤로</button>
+          <button onClick={this.backward}>뒤로</button>
+          <button>
+            {year}/{month + 1}
+          </button>
+          <button onClick={this.forward}>앞으로</button>
         </div>
         <div className="day-title">
           <div className="date-row">
             {this.week.map(day => (
-              <div className="date-cell">{day}</div>
+              <div key={day} className="date-cell">
+                {day}
+              </div>
             ))}
           </div>
         </div>
 
-        {arrays.map(array => (
-          <div className="date-row">
+        {arrays.map((array, i) => (
+          <div key={i} className="date-row">
             {array.map(
-              num =>
-                num.month ? (
-                  <div className="date-cell last-month ">{num.day}</div>
+              (num, i) =>
+                num.day ? (
+                  <div key={i} className="date-cell last-month ">
+                    {num.day}
+                  </div>
                 ) : (
-                  <div className="date-cell">{num}</div>
+                  <div key={i} className="date-cell">
+                    {num}
+                  </div>
                 )
             )}
           </div>
