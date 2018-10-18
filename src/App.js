4c90b24a51572b73daacf7a2868efcac5002bc84
koobohyeon <goosee115@gmail.com>
2018-10-18 23:27:07 +0900

    스타일 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import Calendar from "./components/Calendar";
 import "./index.css";
+const events = [
+  {
+    id: 1,
+    time: 1539854335,
+    title: "빵먹고싶빵"
+  },
+  {
+    id: 2,
+    time: 1538956800,
+    title: "하하하 "
+  }
+];
 class App extends Component {
   render() {
-    return <Calendar />;
+    return <Calendar events={events} />;
   }
 }
 
